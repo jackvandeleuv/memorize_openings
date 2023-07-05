446b22414dcc3b47b19eee14931a3fecc2aae078
Jack Vandeleuv <jvandele@andrew.cmu.edu>
2023-07-05 16:13:53 -0400

    chore: updated home and about page content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,18 +6,31 @@ const About: React.FC = () => {
 	return (
 		<div className="flex flex-col bg-indigo-400 w-full">
 
-			<div className="py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[400px_minmax(500px,_1fr)] bg-indigo-500">
-				<h2 className="flex justify-center items-center px-10 text-xl sm:text-2xl lg:text-3xl font-bold text-center">About Fried Liver</h2>
-				<p className="flex items-center px-6 text-base sm:text-lg lg:text-xl text-gray-700">
-					Fried Liver is an application designed to help users improve their chess skills. It teaches chess openings through spaced repetition, which is a well-studied and effective method for memorization.
+			<div className="py-16 grid grid-cols-1 bg-indigo-500">
+				<h2 className="pb-6 flex justify-center items-center px-10 text-xl sm:text-2xl lg:text-3xl font-bold text-center">About Fried Liver</h2>
+				<p className="px-6 text-base sm:text-lg lg:text-xl text-gray-700">
+					Fried Liver is an application designed to help users improve their chess skills. It teaches chess openings through spaced repetition, which is a well-studied and effective method for memorization. Fried Liver uses Anki&apos;s implementation of the SuperMemo 2 spaced-repetition algorithm. Learn more about <a href='https://faqs.ankiweb.net/what-spaced-repetition-algorithm.html' className='text-indigo-200 hover:underline'>the original algorithm.</a>
 				</p>
-
 			</div>
 
-			<div className="py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[400px_minmax(500px,_1fr)] bg-indigo-400">
-				<h3 className="flex justify-center items-center px-10 text-xl sm:text-2xl lg:text-3xl font-bold text-center">Contact Us</h3>
-				<p className="flex items-center px-6 text-base sm:text-lg lg:text-xl text-gray-700">
-					If you have any questions or feedback, please contact us at <a href="mailto:info@friedliver.com" className="text-indigo-400">info@friedliver.com</a>.
+			<div className="py-16 grid grid-cols-1 bg-indigo-400">
+				<h2 className="pb-6 flex justify-center items-center px-10 text-xl sm:text-2xl lg:text-3xl font-bold text-center">Achieving Comprehensive Opening Coverage</h2>
+				<p className="px-6 text-base sm:text-lg lg:text-xl text-gray-700">
+					The challenge with most traditional methods of learning opening theory is that the positions you study will rarely be seen in an actual game. For example, if the position you are studying occurs at move 10, then to reach that position requires both you and your opponent to each make five specific moves. For most chess players, this won&apos;t be a reality. Even if you are well-prepared, chances are your opponent won&apos;t be.
+					<br /><br />
+					Fried Liver addresses this issue by using data from Lichess to find the moves you are most likely to encounter. The current set of opening decks each offer 80% coverage, meaning that for any given move you practice, you&apos;ll also be learning enough variations to cover 80% of the most popular ways for your opponents to respond. Currently, this is calibrated to 1600-level Lichess players, although future versions will offer more options for hypothetical opponent strength.
+					<br /><br />
+					If you wanted to learn every possible response by your opponent, you would have to memorize a few million positions for every opening. By focusing on 80% coverage, we cut that number down dramatically to a few thousand (or hundred in many cases).
+					<br /><br />
+					The downside is that the likelihood of your opponent making a move you haven&apos;t studied increases as the game goes on. If, on the third move, your opponent has a 80% chance of making a familiar move, then in 20% of games your opponent will surprise you. Let&apos;s say that for each of these moves by your opponent, you learn a response. Now, your opponent moves again (move 5), and the probability you are still within your opening preparation drops to 64% (0.8 * 0.8). By move 13, only 25% of your opponents moves will be familiar to you. However, that is still ten moves where you are making perfect moves, and your opponent can blunder!
+				</p>
+			</div>
+
+			<div className="py-16 grid grid-cols-1 bg-indigo-500">
+				<h3 className="pb-6 flex justify-center items-center px-10 text-xl sm:text-2xl lg:text-3xl font-bold text-center">Contact Us</h3>
+				<p className="px-6 text-base sm:text-lg lg:text-xl text-gray-700">
+					If you have any questions or feedback, please forward them to&nbsp;
+					<a href="mailto:jack.vandeleuv@gmail.com" className='text-indigo-200 hover:underline'>jack.vandeleuv@gmail.com.</a>
 				</p>
 			</div>
 		</div>
--- app/page.tsx
+++ app/page.tsx
@@ -7,31 +7,31 @@ import Footer from "./Footer";
 const HomePage: React.FC = () => {
 	return (
 		<div className="flex flex-col bg-indigo-400 w-full">
-				<div className="py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[400px_minmax(500px,_1fr)] bg-indigo-500">
+				<div className="py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[400px_minmax(500px,_1fr)] bg-indigo-400 gap-2">
 					<h2 className="flex justify-center items-center px-10 text-xl sm:text-2xl lg:text-3xl font-bold text-center">Welcome to Fried Liver</h2>
 					<p className="flex items-center px-6 text-base sm:text-lg lg:text-xl text-gray-700">
-						Fried Liver is an application inspired by Anki, tailored for chess enthusiasts to review chess positions in various openings.
+					{"Fried Liver is an innovative platform for learning chess openings, inspired by Anki's spaced repetition system. Its unique approach focuses on helping you master your favorite chess openings one move at a time, storing them in your long-term memory for a stronger gameplay."}
 					</p>
 				</div>
 
-				<div className="py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[400px_minmax(500px,_1fr)] bg-indigo-400">
-					<h2 className="flex justify-center items-center px-10 text-xl sm:text-2xl lg:text-3xl font-bold text-center">Features</h2>
+				<div className="py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[400px_minmax(500px,_1fr)] bg-indigo-500 gap-2">
+					<h2 className="flex justify-center items-center px-10 text-xl sm:text-2xl lg:text-3xl font-bold text-center">How It Works</h2>
 					<p className="flex items-center px-6 text-base sm:text-lg lg:text-xl text-gray-700">
-						With Fried Liver, you can practice and memorize your favorite chess openings, learn new ones, and test your knowledge with quizzes and puzzles.
+						{"The better you get at recognizing a position, the less often you'll see it. But if you get it wrong, we'll show it again soon, until you know it by heart."}
 					</p>
 				</div>
 
-				<div className="py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[400px_minmax(500px,_1fr)] bg-indigo-500">
-					<h2 className="flex justify-center items-center px-10 text-xl sm:text-2xl lg:text-3xl font-bold text-center">How it Works</h2>
+				<div className="py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[400px_minmax(500px,_1fr)] bg-indigo-400 gap-2">
+					<h2 className="flex justify-center items-center px-10 text-xl sm:text-2xl lg:text-3xl font-bold text-center">Optimized Opening Preparation</h2>
 					<p className="flex items-center px-6 text-base sm:text-lg lg:text-xl text-gray-700">
-						{"Just like Anki, Fried Liver uses spaced repetition to help you remember chess openings. The more you get a position right, the less you'll see it. But if you get it wrong, we'll show it again soon, until you know it by heart."}
+						{"Fried Liver uses analytics from the Lichess player API to create the most useful possible opening preparation. For every move you learn, we make sure that 80% of your real-life opponents' responses are covered by your learning material"}
 					</p>
 				</div>
 
-				<div className="py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[400px_minmax(500px,_1fr)] bg-indigo-400">
+				<div className="py-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[400px_minmax(500px,_1fr)] bg-indigo-500 gap-2">
 					<h2 className="flex justify-center items-center px-10 text-xl sm:text-2xl lg:text-3xl font-bold text-center">Get Started</h2>
 					<p className="flex items-center px-6 text-base sm:text-lg lg:text-xl text-gray-700">
-						Ready to level up your chess game? Sign up today and start mastering chess openings with Fried Liver.
+						{"Start your journey with Fried Liver today. Sign up for a free account and begin your journey to opening mastery!"}
 					</p>
 				</div>
 			</div>
